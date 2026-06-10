aaf57271c455c53e756ca6ba1cd3dcd9908e85d3
Kalluri Rakesh <kalluirakesh9@gmail.com>
2026-06-10 12:17:01 +0530

    Sprint 9 Backend Complete - Personalized Feed

--- posts/views.py
+++ posts/views.py
@@ -14,14 +14,43 @@ from .models import Post
 
 from .serializers import PostSerializer
 
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Post
+from .serializers import PostSerializer
+
 
 class PostListView(
     generics.ListAPIView
 ):
 
-    queryset = Post.objects.all()
+    serializer_class = (
+        PostSerializer
+    )
 
-    serializer_class = PostSerializer
+    permission_classes = [
+        IsAuthenticated
+    ]
+
+    def get_queryset(self):
+
+        user = self.request.user
+
+        followed_users = (
+            user.following.all()
+        )
+
+        return Post.objects.filter(
+
+            author__in=(
+                list(followed_users)
+                + [user]
+            )
+
+        ).order_by(
+            "-created_at"
+        )
 
 
 class PostDetailView(
