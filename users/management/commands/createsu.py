b4d0194528d5aa556bf1bdf773d52947b31c11dd
Kalluri Rakesh <kalluirakesh9@gmail.com>
2026-06-12 12:14:22 +0530

    feat: add createsu management command

---
+++ users/management/commands/createsu.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        username = "rakesh"
+        password = "rakesh123"
+        email = "rakesh@example.com"
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password
+            )
+            self.stdout.write("Superuser created successfully")
+        else:
+            self.stdout.write("Superuser already exists")
