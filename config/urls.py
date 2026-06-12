bc32df750e2e7213d1aaad9b8bdc5875d3dfb16a
Kalluri Rakesh <kalluirakesh9@gmail.com>
2026-06-12 13:43:43 +0530

    Fix media serving

--- config/urls.py
+++ config/urls.py
@@ -37,9 +37,7 @@ path(
 ),
 ]
 
-if settings.DEBUG:
-
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
